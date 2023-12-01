d604cd90903cd27239d9897b2ebcabb69267d198
Fredrik Blom <fredrik@familjenblom.se>
2023-12-01 07:50:52 +0100

    Override theme color for 'n'

--- Solutions/Y2023/Theme.cs
+++ Solutions/Y2023/Theme.cs
@@ -4,6 +4,7 @@ class Theme : ITheme
 {
     public Dictionary<string, int> Override(Dictionary<string, int> themeColors)
     {
+        themeColors["calendar-color-n"] = 0x9b715b;
         return themeColors;
     }
 }
