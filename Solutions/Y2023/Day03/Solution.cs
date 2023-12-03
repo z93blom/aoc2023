d058c0c7d6c9b8dc24eb65cb81584af30f80efd2
Fredrik Blom <fredrik@familjenblom.se>
2023-12-03 09:04:19 +0100

    Day 3, part 2 solved

--- Solutions/Y2023/Day03/Solution.cs
+++ Solutions/Y2023/Day03/Solution.cs
@@ -89,6 +89,80 @@ class Solution : ISolver
 
     static object PartTwo(string input, Func<TextWriter> getOutputFunction)
     {
-        return 0;
+        var lines = input
+            .Lines()
+            .ToArray();
+
+        var grid = new Grid<char>(lines[0].Length, lines.Length, YAxisDirection.ZeroAtTop);
+        var numberPoints = new HashSet<Point2>();
+        var symbolPoints = new HashSet<Point2>();
+        var lastDigits = new Dictionary<Point2, Point2>();
+        for (var y = 0; y < lines.Length; y++)
+        {
+            var line = lines[y];
+            for (var x = 0; x < line.Length; x++)
+            {
+                var c = line[x];
+                var p = new Point2(x, y, YAxisDirection.ZeroAtTop);
+                grid[p] = c;
+
+                if (char.IsDigit(c))
+                {
+                    numberPoints.Add(p);
+                }
+                else if (c == '*')
+                {
+                    symbolPoints.Add(p);
+                }
+            }
+        }
+
+        foreach (var point in numberPoints)
+        {
+            lastDigits[point] = GetLastDigit(grid, point);
+        }
+        
+        var sum = 0L;
+        foreach (var symbolPoint in symbolPoints)
+        {
+            var adjacentNumbers = symbolPoint
+                .AdjacentPoints
+                .Where(p => grid.Contains(p) && char.IsDigit(grid[p]))
+                .Select(p => lastDigits[p])
+                .Distinct()
+                .ToArray();
+            if (adjacentNumbers.Length == 2)
+            {
+                var first = GetValue(grid, adjacentNumbers[0]);
+                var second = GetValue(grid, adjacentNumbers[1]);
+                sum += first * second;
+            }
+        }
+
+        return sum;
+    }
+
+    private static Point2 GetLastDigit(Grid<char> grid, Point2 p)
+    {
+        var c = p;
+        while (grid.Contains(c.Right) && char.IsDigit(grid[c.Right]))
+            c = c.Right;
+
+        return c;
+    }
+
+    private static int GetValue(Grid<char> grid, Point2 p)
+    {
+        var c = p;
+        var value = grid[c] - '0';
+        var factor = 1;
+        while (grid.Contains(c.Left) && char.IsDigit(grid[c.Left]))
+        {
+            c = c.Left;
+            factor *= 10;
+            value += factor * (grid[c] - '0');
+        }
+
+        return value;
     }
 }
