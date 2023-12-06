469fe8d3f9f50f1da9203325d82e3e22550d8512
Fredrik Blom <fredrik@familjenblom.se>
2023-12-06 17:52:54 +0100

    Got the right value for the example for day 5, part 2. Not correct for my value though

--- Solutions/Y2023/Day05/Solution.cs
+++ Solutions/Y2023/Day05/Solution.cs
@@ -48,7 +48,7 @@ class Solution : ISolver
                 index++;
             }
 
-            var map = new Map(names[0].Value, names[1].Value, mappers.ToArray());
+            var map = new Map(names[0].Value, names[1].Value, mappers.OrderBy(m => m.Source.Start).ToArray());
             maps.Add(map);
             index++;
         }
@@ -78,21 +78,114 @@ class Solution : ISolver
 
             return seed;
         }
+
+        public IEnumerable<RangeL> MapRange(RangeL range)
+        {
+            var mappers = GetMappers(range).ToArray();
+            if (mappers.Length == 0)
+            {
+                yield return range;
+                yield break;
+            }
+
+            var r = range;
+            var index = 0;
+            while (index < mappers.Length && !r.IsEmpty)
+            {
+                var mapper = mappers[index];
+                if (r.Start > mapper.Source.Last)
+                {
+                    index++;
+                    continue;
+                }
+
+                RangeL left;
+                if (r.Start < mapper.Source.Start)
+                {
+                    // The range sticks out to the left of the current range.
+                    (left, r) = r.Split(mapper.Source.Start);
+                    yield return left;
+                    continue;
+                }
+
+                (left, r) = r.Split(mapper.Source.Last + 1);
+                yield return left with {Start = mapper.MapValue(left.Start)};
+            }
+
+            if (!r.IsEmpty)
+            {
+                yield return r;
+            }
+        }
+
+        private IEnumerable<Mapper> GetMappers(RangeL range)
+        {
+            return Mappers.Where(mapper => mapper.Source.Intersects(range));
+        }
     }
 
-    private record struct Mapper(long Destination, RangeL Source)
+    private readonly record struct Mapper(long Destination, RangeL Source)
     {
-        public RangeL DestinationRange = Source with { Start = Destination };
-
-        private long MapDestinationToSource(long position)
+        public long MapValue(long value)
         {
-            return position - Destination + Source.Start;
+            if (value < Source.Start || value > Source.Last)
+            {
+                return value;
+            }
+
+            return value - Source.Start + Destination;
+        }
+
+        public IEnumerable<RangeL> MapRange(RangeL range)
+        {
+            var totalLength = 0L;
+            if (!range.Intersects(Source))
+            {
+                yield return range;
+                totalLength = range.Length;
+            }
+            else if (range.Start < Source.Start && range.Last > Source.Last)
+            {
+                var (left, rest) = range.Split(Source.Start);
+                var (middle, right) = rest.Split(Source.Last + 1);
+                yield return left;
+                yield return middle with { Start = MapValue(middle.Start) };
+                yield return right;
+                totalLength = left.Length + middle.Length + right.Length;
+            }
+            else if (range.Start < Source.Start)
+            {
+                var (left, right) = range.Split(Source.Start);
+                yield return left;
+                yield return right with { Start = MapValue(right.Start) };
+                totalLength = left.Length + right.Length;
+            }
+            else if (range.Last > Source.Last)
+            {
+                var (left, right) = range.Split(Source.Last + 1);
+                yield return left with { Start = MapValue(left.Start) };
+                yield return right;
+                totalLength = left.Length + right.Length;
+            }
+            else
+            {
+                yield return range with { Start = MapValue(range.Start) };
+                totalLength = range.Length;
+            }
+
+            if (totalLength != range.Length)
+            {
+                throw new Exception("Implementation error");
+            }
         }
     }
 
     private readonly record struct RangeL(long Start, long Length)
     {
+        public static RangeL Empty => new RangeL(0, 0); 
         public long Last => Start + Length - 1;
+
+        public bool IsEmpty => Length <= 0;
 
         public bool Intersects(RangeL other)
         {
@@ -105,17 +198,11 @@ class Solution : ISolver
             return true;
         }
 
-        public RangeL Intersection(RangeL other)
-        {
-            var start = other.Start > Start ? other.Start : Start;
-            var last = other.Last < Last ? other.Last : Last;
-            return new RangeL(start, last - start); // Length might be off by one, but I don't care.
-        }
-
         public (RangeL Left, RangeL Right) Split(long firstRightPosition)
         {
-            var left = this with { Length = firstRightPosition - Start };
-            var right = new RangeL(firstRightPosition, Length - left.Length);
+            var actualLast = Math.Min(Last, firstRightPosition - 1);
+            var left = this with { Length = actualLast + 1 - Start };
+            var right = new RangeL(actualLast + 1, Length - left.Length);
             return (left, right);
         }
     }
@@ -161,7 +248,41 @@ class Solution : ISolver
             maps.Add(map);
             index++;
         }
+
+
+        var rangesToTry = new Stack<RangeAndLevel>();
         
-        return 0;
+        foreach (var seedRange in seedRanges)
+        {
+            rangesToTry.Push(new RangeAndLevel(seedRange, 0));
+        }
+
+        var min = long.MaxValue;
+        while (rangesToTry.Count > 0)
+        {
+            var (range, level) = rangesToTry.Pop();
+            if (level >= maps.Count)
+            {
+                if (range.Start < min)
+                {
+                    min = range.Start;
+                }
+            }
+            else
+            {
+                var map = maps[level];
+                foreach (var r in map.MapRange(range))
+                {
+                    rangesToTry.Push(new RangeAndLevel(r, level + 1));
+                }
+            }
+        }
+        
+        return min;
+    }
+
+    private readonly record struct RangeAndLevel(RangeL Range, int Level)
+    {
+        public void Deconstruct(out RangeL range, out int level) => (range, level) = (Range, Level);
     }
 }
