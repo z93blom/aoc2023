2d5ff29d1d1d552df7c258c89bff5376b1cb3834
Fredrik Blom <fredrik@familjenblom.se>
2023-12-06 13:13:02 +0100

    Removed the bottom-up solution.

--- Solutions/Y2023/Day05/Solution.cs
+++ Solutions/Y2023/Day05/Solution.cs
@@ -84,47 +84,11 @@ class Solution : ISolver
     {
         public RangeL DestinationRange = Source with { Start = Destination };
 
-        public IEnumerable<RangeLAndInfo> MapDestinationRangeToSource(RangeL range)
-        {
-            if (range.Start < DestinationRange.Start && range.Last > DestinationRange.Last)
-            {
-                // Three ranges - left, mapped, right
-                var (left, _) = range.Split(DestinationRange.Start);
-                yield return new RangeLAndInfo(left, true);
-
-                yield return new RangeLAndInfo(Source, false);
-                
-                var (_, right) = range.Split(DestinationRange.Last + 1);
-                yield return new RangeLAndInfo(right, true);
-            }
-            else if (range.Start < DestinationRange.Start)
-            {
-                // Two - left and mapped
-                var (left, _) = range.Split(DestinationRange.Start);
-                yield return new RangeLAndInfo(left, true);
-                yield return new RangeLAndInfo(new RangeL(MapDestinationToSource(DestinationRange.Start), range.Length - left.Length), false);
-            }
-            else if (range.Last < DestinationRange.Last)
-            {
-                // Two - mapped and right
-                var (_, right) = range.Split(DestinationRange.Last + 1);
-                yield return new RangeLAndInfo(new RangeL(MapDestinationToSource(range.Start), range.Length - right.Length), false);
-                yield return new RangeLAndInfo(right, true);;
-            }
-            else
-            {
-                // completely within
-                yield return new RangeLAndInfo(range with { Start = MapDestinationToSource(range.Start) }, false);
-            }
-        }
-
         private long MapDestinationToSource(long position)
         {
             return position - Destination + Source.Start;
         }
     }
-
-    private readonly record struct RangeLAndInfo(RangeL Range, bool IsFallthrough);
 
     private readonly record struct RangeL(long Start, long Length)
     {
@@ -198,77 +162,6 @@ class Solution : ISolver
             index++;
         }
         
-        // Start from the end, work your way back up to the top, and see if there are any seeds that fall into
-        // the correct bucket.
-
-        var toTry = new Stack<RangeAndLevel>();
-        foreach (var mapper in maps[^1].Mappers.Reverse())
-        {
-            toTry.Push(new RangeAndLevel(mapper.Source, 5));
-        }
-        
-        if (maps[^1].Mappers[0].Destination != 0)
-        {
-            toTry.Push(new RangeAndLevel(new RangeL(0, maps[^1].Mappers[0].Destination), 5));
-        }
-
-        var startingSeed = -1L;
-        while (toTry.Count > 0)
-        {
-            var rl = toTry.Pop();
-
-            if (rl.Level < 0)
-            {
-                // try the seeds.
-                var seedRangesThatIntersect = seedRanges
-                    .Where(r => rl.Range.Intersects(r))
-                    .OrderBy(r => r.Start)
-                    .ToArray();
-                if (seedRangesThatIntersect.Length > 0)
-                {
-                    var intersection = rl.Range.Intersection(seedRangesThatIntersect[0]);
-                    startingSeed = intersection.Start;
-                    break;
-                }
-            }
-            else
-            {
-                // map another level up.
-                var mappers = maps[rl.Level].Mappers
-                    .Where(m => rl.Range.Intersects(m.DestinationRange))
-                    .OrderByDescending(m => m.Destination)
-                    .ToArray();
-                if (mappers.Length == 0)
-                {
-                    // No mappers on this level intersect the current destination range.
-                    // Just go up a level.
-                    toTry.Push(rl with { Level = rl.Level - 1 });
-                }
-                else
-                {
-                    foreach (var mapper in mappers)
-                    {
-                        // Here's a bug - we can only map a value at most once. Right now we're
-                        // taking all ranges from all mappers, even those that might be mapped
-                        // with another mapper.
-                        foreach (var sr in mapper.MapDestinationRangeToSource(rl.Range).Reverse())
-                        {
-                            toTry.Push(new RangeAndLevel(sr.Range, rl.Level - 1));
-                        }
-                    }
-                }
-            }
-        }
-
-        if (startingSeed < 0)
-        {
-            throw new Exception("Unable to solve this.");
-        }
-        
-        // Get the final location for the seed.
-        var final = maps.Aggregate(startingSeed, (s, map) => map.MapSeed(s));
-        return final;
+        return 0;
     }
-
-    private readonly record struct RangeAndLevel(RangeL Range, int Level);
 }
