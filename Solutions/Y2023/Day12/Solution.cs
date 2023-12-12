96c5d91e6c8e7337951a24b2d473d96364f91d25
Fredrik Blom <fredrik@familjenblom.se>
2023-12-12 09:50:32 +0100

    This should solve Day 12, part 2

--- Solutions/Y2023/Day12/Solution.cs
+++ Solutions/Y2023/Day12/Solution.cs
@@ -1,5 +1,6 @@
 using AdventOfCode.Framework;
 using AdventOfCode.Utilities;
+using LanguageExt.UnitsOfMeasure;
 using Spectre.Console;
 
 namespace AdventOfCode.Solutions.Y2023.Day12;
@@ -137,6 +138,121 @@ class Solution : ISolver
 
     static object PartTwo(string input, Func<TextWriter> getOutputFunction)
     {
-        return 0;
+        var rows = input.SplitBySingleNewline()
+            .Select(s => s.Split(" ").ToArray())
+            .Select(a => new Row(a[0].ToCharArray(), a[1].Integers().ToArray()))
+            .Select(r => new Row(
+                string.Join('?', Enumerable.Range(0, 5).Select(_ => new string(r.Record))).ToCharArray(), 
+                Enumerable.Range(0, 5).Aggregate(Array.Empty<int>(), (c, _) => c.Concat(r.Conditions).ToArray())))
+            .ToArray();
+
+        var arrangements = new List<long>();
+        foreach (var row in rows)
+        {
+            var validArrangements = GetValidPermutations(row, row.Record, row.Conditions, 0).ToArray();
+            arrangements.Add(validArrangements.Length);
+        }
+        
+        return arrangements.Sum();
+    }
+
+    private static IEnumerable<char[]> GetValidPermutations(Row row, char[] input, int[] conditions, int index)
+    {
+        if (row.Record.Length != input.Length)
+        {
+            yield break;
+        }
+        
+        if (index == input.Length)
+        {
+            if (IsValid(input, conditions))
+            {
+                yield return input;
+            }
+
+            yield break;
+        }
+
+        if (input[index] != '?')
+        {
+            foreach (var p in GetValidPermutations(row, input, conditions, index + 1))
+            {
+                yield return p;
+            }
+        }
+        else
+        {
+            var start = input[..index];
+            if (!StillValid(start, conditions))
+            {
+                yield break;
+            }
+
+            var withPeriod = start.Concat(Period).Concat(input[(index + 1)..]).ToArray();
+            foreach (var p in GetValidPermutations(row, withPeriod, conditions, index + 1))
+            {
+                yield return p;
+            }
+
+            var withHash = start.Concat(Hash).Concat(input[(index + 1)..]).ToArray();
+            foreach (var p in GetValidPermutations(row, withHash, conditions, index + 1))
+            {
+                yield return p;
+            }
+        }
+    }
+    
+    private static bool StillValid(char[] input, int[] conditions)
+    {
+        var groupIndex = 0;
+        var i = 0;
+        var isInGroup = false;
+        var groupLength = 0;
+        while (i < input.Length)
+        {
+            if (input[i] == '#' && isInGroup)
+            {
+                groupLength++;
+                if (groupLength > conditions[groupIndex])
+                {
+                    // Invalid length of this group.
+                    return false;
+                }
+            }
+            else if (input[i] == '#')
+            {
+                isInGroup = true;
+                groupLength = 1;
+                if (groupIndex == conditions.Length)
+                {
+                    // Too many groups.
+                    return false;
+                }
+            }
+            else if (isInGroup)
+            {
+                isInGroup = false;
+                if (groupLength != conditions[groupIndex])
+                {
+                    // Invalid length of this group.
+                    return false;
+                }
+
+                groupIndex++;
+            }
+
+            i++;
+        }
+
+        if (isInGroup)
+        {
+            if (groupLength > conditions[groupIndex])
+            {
+                // Invalid length of this group.
+                return false;
+            }
+        }
+
+        return true;
     }
 }
