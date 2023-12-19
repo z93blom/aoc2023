f146d992760fc882e68954dac796a80ccf0fedd4
Fredrik Blom <fredrik@familjenblom.se>
2023-12-19 08:17:07 +0100

    Reaches an answer in acceptable time, but it is not correct.

--- Solutions/Y2023/Day19/Solution.cs
+++ Solutions/Y2023/Day19/Solution.cs
@@ -1,3 +1,4 @@
+using System.Net.WebSockets;
 using AdventOfCode.Framework;
 using AdventOfCode.Utilities;
 using LanguageExt;
@@ -126,9 +127,141 @@ class Solution : ISolver
     }
     
     
+    private readonly record struct Workflow2(string Name, Dictionary<string, Condition2[]> Conditions);
+
+    private readonly record struct Condition2(bool DefaultWorkflow, char Property, char Comparer, int Value, string Target)
+    {
+        public static Condition2 Parse(string arg)
+        {
+            var colonIndex = arg.IndexOf(':');
+            if (colonIndex < 0)
+            {
+                // default condition
+                return new Condition2(true, ' ', ' ', 0, arg);
+            }
+
+            var property = arg[0];
+            var comparer = arg[1];
+            var number = int.Parse(arg[2..colonIndex]);
+            var target = arg[(colonIndex + 1)..];
+            return new Condition2(false, property, comparer, number, target);
+        }
+    }
 
     static object PartTwo(string input, Func<TextWriter> getOutputFunction)
     {
-        return 0;
+        var a = input.SplitByDoubleNewline().ToArray();
+        Dictionary<string, Workflow2> workflows = new();
+        foreach (var s in a[0].SplitBySingleNewline())
+        {
+            var groups = s.Groups(@"(\w+)\{(.*)\}").First();
+            var name = groups[0].Value;
+            var conditions = groups[1].Value
+                .Split(',')
+                .Select(Condition2.Parse)
+                .ToArray();
+            
+            workflows.Add(name, new Workflow2(name, 
+                conditions.GroupBy(c => c.Target).ToDictionary(g => g.Key, g => g.ToArray())));
+        }
+        
+        // We want to find all the combinations that will end up in state "A".
+        var conditionsToReachA = new Dictionary<string, List<ConditionsToReachA>>();
+        
+        // Start with all the workflows that end up in A, and then work backwards.
+        Queue<string> targetWorkflows = new();
+        targetWorkflows.Enqueue("A");
+        conditionsToReachA["A"] = new List<ConditionsToReachA>(new []{ new ConditionsToReachA(Array.Empty<Condition2>()) });
+        while (targetWorkflows.Count > 0)
+        {
+            var targetWorkflow = targetWorkflows.Dequeue();
+            foreach (var wf in workflows.Values.Where(wf => wf.Conditions.ContainsKey(targetWorkflow)))
+            {
+                if (conditionsToReachA.ContainsKey(wf.Name))
+                {
+                    continue;
+                }
+                
+                var previousConditions = conditionsToReachA[targetWorkflow];
+                if (wf.Conditions[targetWorkflow].Any(c => c.DefaultWorkflow))
+                {
+                    // No new conditions need to be added.
+                    // Just copy the conditions from the target workflow.
+                    conditionsToReachA[wf.Name] = new List<ConditionsToReachA>(previousConditions);
+                }
+                else
+                {
+                    conditionsToReachA[wf.Name] = new List<ConditionsToReachA>();
+                    foreach (var newCondition in wf.Conditions[targetWorkflow])
+                    {
+                        foreach (var previousCondition in previousConditions)
+                        {
+                            conditionsToReachA[wf.Name].Add(previousCondition.AddCondition(newCondition));
+                        }
+                    }
+                }
+                
+                targetWorkflows.Enqueue(wf.Name);
+            }
+        }
+
+        var conditionsForIn = conditionsToReachA["in"];
+        var acceptableConditions = 0L;
+        foreach (var cdns in conditionsForIn)
+        {
+            acceptableConditions += cdns.AcceptableConditions();
+        }
+
+        return acceptableConditions;
+    }
+
+    private readonly struct ConditionsToReachA(Condition2[] Conditions)
+    {
+        public ConditionsToReachA AddCondition(Condition2 condition)
+        {
+            if (condition.DefaultWorkflow)
+            {
+                return this;
+            }
+
+            return new ConditionsToReachA(Conditions.Append(condition).ToArray());
+        }
+
+        public long AcceptableConditions()
+        {
+            var groups = Conditions.GroupBy(c => c.Property);
+            var allowedRanges = new List<int>();
+            foreach (var group in groups)
+            {
+                var minValue = 1;
+                var maxValue = 4000;
+                foreach (var c in group)
+                {
+                    if (c.Comparer == '<')
+                    {
+                        maxValue = Math.Min(c.Value, maxValue);
+                    }
+                    else if (c.Comparer == '>')
+                    {
+                        minValue = Math.Max(c.Value, minValue);
+                    }
+                    else
+                    {
+                        throw new Exception("Invalid comparer");
+                    }
+                }
+
+                if (minValue <= maxValue)
+                {
+                    allowedRanges.Add(maxValue - minValue + 1);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return allowedRanges.Aggregate(1L, (v, i) => v * i);
+        }
     }
 }
